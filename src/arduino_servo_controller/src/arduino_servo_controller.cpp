e1699469f0c9ffb9d9d44279def51f3524a859ba
NaokiAkai <n.akai.goo@gmail.com>
2020-12-22 10:01:00 +0900

    add angular_velocity_controller.cpp

--- src/arduino_servo_controller/src/arduino_servo_controller.cpp
+++ src/arduino_servo_controller/src/arduino_servo_controller.cpp
@@ -2,18 +2,18 @@
  * ROSのお勉強用プログラムです．
  * どなたでもご自由にお使いください．
  * 作者: 赤井直紀
+ *
+ * これはArduinoにサーボモータが繋がっている前提のプロフラムです．
+ * なお，Arduinoに書き込まれているプログラムは以下になります．
+ * ros_intro/arduino/ServoVelocityControl/ServoVelocityControl.ino
  */
-
-// Arduinoにサーボモータが繋がっている前提のプロフラムです．
-// Arduinoに書き込まれているプログラムは以下になります．
-// ros_intro/arduino/ServoVelocityControl/ServoVelocityControl.ino
 
 #include <ros/ros.h>
 
 // サーボモータへ速度の指令はgeometry_msgs::TwistStampedで受け取ります．
 #include <geometry_msgs/TwistStamped.h>
 
-// シリアル通信のためのヘッダファイルをインクルードします．
+// シリアル通信（Arduinoと通信するため）のためのヘッダファイルをインクルードします．
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -28,7 +28,7 @@ private:
     ros::Subscriber twistSub_;
     std::string inputTopicName_;
 
-    // // シリアル通信設定のためのパラメータです．
+    // シリアル通信設定のためのパラメータです．
     int fd_;
     struct termios tio_;
     std::string device_;
@@ -37,16 +37,17 @@ private:
 public:
     ArduinoServoController(void):
         nh_("~"),
-        inputTopicName_("/servo_angular_velocities"),
+        inputTopicName_("/servo_motor_twist_cmd"),
         device_("/dev/ttyACM0"),
         baudRate_(B9600)
     {
+        nh_.param("servo_motor_twist_cmd", inputTopicName_, inputTopicName_);
         nh_.param("arduino_device", device_, device_);
         nh_.param("arduino_baud_rate", baudRate_, baudRate_);
 
         twistSub_ = nh_.subscribe(inputTopicName_, 1, &ArduinoServoController::twistCB, this);
 
-        // デバイスを開き，シリアル通信の設定をします．
+        // デバイスを開きシリアル通信の設定をします．
         fd_ = open(device_.c_str(), O_RDWR);
         if (fd_ < 0) {
             fprintf(stderr, "cannot open the serial port -> %s\n", device_.c_str());
@@ -62,7 +63,9 @@ public:
         cfmakeraw(&tio_);
         tcsetattr(fd_, TCSANOW, &tio_);
         ioctl(fd_, TCSETS, &tio_);
-        sleep(1);
+
+        // デバイスを開いてから少し待機する（開いてからすぐは通信できない？）
+        sleep(2);
     }
 
     ~ArduinoServoController(void) {
@@ -88,17 +91,16 @@ void ArduinoServoController::twistCB(const geometry_msgs::TwistStamped::ConstPtr
         isFirst = false;
     }
 
-    // 書き込みの間隔が短い場合には書き込みをしません．
-    // Arduinoが追いつけない？
+    // 書き込みの間隔が短い場合には書き込みをしません（Arduinoが追いつけない？）．
     double currTime = msg->header.stamp.toSec();
     double deltaTime = currTime - prevTime;
-    if (deltaTime < 0.01)
+    if (deltaTime < 0.1)
         return;
 
     // サーボモータへの速度指令を書き込みます．
     // yaw軸方向の速度のみを利用します．
+    // cmdの最後の'\0'は，角速度を表す文字列の最後を意味する文字となっています．
     int yawAngVel = (int)(msg->twist.angular.z);
-    // '\0'がコマンドの最後を意味する文字となっています．
     std::string cmd = std::to_string(yawAngVel) + '\0';
     int retVal = write(fd_, cmd.c_str(), cmd.length());
     prevTime = currTime;
---
+++ src/realsense_examples/src/angular_velocity_controller.cpp
@@ -0,0 +1,146 @@
+/*
+ * ROSのお勉強用プログラムです．
+ * どなたでもご自由にお使いください．
+ * 作者: 赤井直紀
+ */
+
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/point_cloud.h>
+
+// 角速度の制御司令はgeometry_msgs::TwistStampedで配信します．
+#include <geometry_msgs/TwistStamped.h>
+
+class AngularVelocityController {
+private:
+    ros::NodeHandle nh_;
+    ros::Subscriber pointsSub_;
+    ros::Publisher twistPub_;
+    std::string inputTopicName_, outputTopicName_;
+
+    // 角速度制御様のパラメータです．
+    float pGain_, iGain_, dGain_;
+    float maxAngVel_;
+
+public:
+    AngularVelocityController(void):
+        nh_("~"),
+        inputTopicName_("/camera/depth_registered/cluster_centroids"),
+        outputTopicName_("/servo_motor_twist_cmd"),
+        pGain_(1.0f),
+        dGain_(0.01f),
+        iGain_(0.001f),
+        maxAngVel_(40.0f)
+    {
+        nh_.param("clustered_centroids_name", inputTopicName_, inputTopicName_);
+        nh_.param("servo_motor_twist_cmd", outputTopicName_, outputTopicName_);
+        nh_.param("servo_controller_p_gain", pGain_, pGain_);
+        nh_.param("servo_controller_i_gain", iGain_, iGain_);
+        nh_.param("servo_controller_d_gain", dGain_, dGain_);
+        nh_.param("servo_controller_max_ang_vel", maxAngVel_, maxAngVel_);
+
+        pointsSub_ = nh_.subscribe(inputTopicName_, 1, &AngularVelocityController::pointsCB, this);
+
+        twistPub_ = nh_.advertise<geometry_msgs::TwistStamped>(outputTopicName_, 1);
+    }
+
+    ~AngularVelocityController(void) {}
+
+    void spin(void);
+
+    void pointsCB(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &msg);
+};
+
+void AngularVelocityController::spin(void) {
+    ros::spin();
+}
+
+// カメラと重心との距離を返します．
+float getDist(pcl::PointXYZ centroid) {
+    return sqrt(centroid.x * centroid.x + centroid.y * centroid.y + centroid.z * centroid.z);
+}
+
+// カメラから最も近い重心を返します．
+pcl::PointXYZ getClosestCentroid(pcl::PointCloud<pcl::PointXYZ> centroids) {
+    // 0番目の重心を最短距離と仮定します．
+    int idx = 0;
+    float minDist = getDist(centroids.points[0]);
+
+    // 1番目以降の重心と比較していきます．
+    for (size_t i = 1; i < centroids.points.size(); i++) {
+        float dist = getDist(centroids.points[i]);
+        // i番目の重心との距離が今の最短距離より近い場合は更新します．
+        if (dist < minDist) {
+            idx = i;
+            minDist = dist;
+        }
+    }
+    return centroids.points[idx];
+} 
+
+void AngularVelocityController::pointsCB(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &msg) {
+    // staic変数はこの関数終了時の最後の値を保存します（最初の値だけ0となります）．
+    static float prevDeltaTheta = 0.0f, deltaThetaSum = 0.0f;
+
+    // pclのメッセージに付属するヘッダの情報をROS様に変換します．
+    std_msgs::Header header;
+    pcl_conversions::fromPCL(msg->header, header);
+
+    // 配信様のメッセージを定義しヘッダの情報を格納します．
+    // frame_idは重要でなく，stampのみが重要になります．
+    geometry_msgs::TwistStamped twistCmd;
+    twistCmd.header = header;
+
+    // 重心がない場合は角速度0を司令して戻ります．
+    if ((int)msg->points.size() == 0) {
+        twistCmd.twist.angular.z = 0.0f;
+        twistPub_.publish(twistCmd);
+        return;
+    }
+
+    // 最も近い重心を取得します．
+    pcl::PointXYZ closestCentroid = getClosestCentroid(*msg);
+
+    // カメラ画像座標をカメラ座標に変換しています．
+    float targetX = closestCentroid.z;
+    float targetY = -closestCentroid.x;
+
+    // 重心とカメラが向いている方向の差分をdegreeで求めます．
+    float deltaTheta = atan2(targetY, targetX) * 180.0f / M_PI;
+
+    // D制御のための値を計算します（角度の差分は必ず−180から180度の値でないといけません）．
+    float dDeltaTheta = deltaTheta - prevDeltaTheta;
+    while (dDeltaTheta < -180.0f)
+        dDeltaTheta += 180.0f;
+    while (dDeltaTheta > 180.0f)
+        dDeltaTheta -= 180.0f;
+
+    // PID制御で角速度を求めます．
+    float angVel = pGain_ * deltaTheta + iGain_ * deltaThetaSum + dGain_ * dDeltaTheta;
+    printf("time = %lf, angVel = %.2f [deg/sec]\n", header.stamp.toSec(), angVel);
+
+    // 配信する前に角速度のチェックを行います（超重要！）．
+    if (std::isnan(angVel))
+        angVel = 0.0f;
+    else if (angVel < -maxAngVel_)
+        angVel = -maxAngVel_;
+    else if (angVel > maxAngVel_)
+        angVel = maxAngVel_;
+    twistCmd.twist.angular.z = angVel;
+    twistPub_.publish(twistCmd);
+
+    // D制御とI制御のためのパラメータを更新します．
+    // これらはstatic変数なので，次にこの関数が始まるときはこの時の値となっています．
+    prevDeltaTheta = deltaTheta;
+    deltaThetaSum += deltaTheta;
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "angular_velocity_controller");
+    AngularVelocityController avc;
+    avc.spin();
+    return 0;
+}
